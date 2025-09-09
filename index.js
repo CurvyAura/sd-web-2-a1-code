d4c6b2a1ce5b256c7845b8fcababea539cb9ea78
Brett <CurvyAura@gmail.com>
2025-09-09 17:12:05 -0600

    Done: exercises 1-2(JS)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 // sample data - expanded Star Wars characters with varied ages
 const users = [
-  { id: 1, name: "Luke Skywalker" age: 23 },
+  { id: 1, name: "Luke Skywalker", age: 23 },
   { id: 2, name: "Darth Vader", age: 45 },
   { id: 3, name: "Princess Leia", age: 23 },
   { id: 4, name: "Obi-Wan Kenobi", age: 57 },
@@ -14,16 +14,47 @@ const users = [
   { id: 10, name: "Padmé Amidala", age: 27 },
 ];
 
+//DOMS
+const namesList = document.getElementById("names-list");
+const youngNamesList = document.getElementById("young-characters-list");
+const functionList = document.getElementById("function-list");
+const ageFilterList = document.getElementById("age-filter-list");
+
+
 // broken test data for exercise 6
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
 
+users.forEach(user => {
+  const li = document.createElement("li");
+
+  console.log(user.name)
+
+  li.textContent = user.name
+  namesList.appendChild(li); // append will attach it to <ul> in the DOM.
+});
+
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
+const filteredNames = users.filter(user => user.age < 40);
+filteredNames.forEach((user) => {
+  const li = document.createElement("li");
+
+  console.log(user.name);
+
+  li.textContent = user.name;
+  youngNamesList.appendChild(li); // append will attach it to <ul> in the DOM.
+});
 
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
 
+
+
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
 
+
+
 // 5. Add error handling to your functions that will log an error message using console.error() if any object doesn't have a "name" property. Display any error messages in the div with id "error-messages"
+
+
 
 // 6. Test your error handling by creating a second array that's intentionally broken (missing name properties) and passing it to your functions. Verify that your error handling works correctly and displays errors in the div with id "broken-array-errors"
